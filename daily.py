dcdfc4a190e5eff8e6fd0505e41f20f27b164fbc
Timothy Poon <timothy.poon520@gmail.com>
2025-04-11 15:46:12 -0700

    support dst

--- daily.py
+++ daily.py
@@ -1,11 +1,12 @@
 from discord.ext import tasks, commands
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
+from zoneinfo import ZoneInfo
 import time
 
 class DailyCog(commands.Cog):
     CHANNEL_ID = 1085319916594405408
     GUILD_ID = 1084587109857370183
-    EVENT_ID = 1216460048843280475
+    EVENT_ID = 1357910907124060281
 
     def __init__(self, bot):
         self.bot = bot
@@ -13,21 +14,23 @@ class DailyCog(commands.Cog):
 
     @tasks.loop(hours=1)
     async def send_reminder(self):
-        channel = self.bot.get_channel(self.CHANNEL_ID)
-        guild = self.bot.get_guild(self.GUILD_ID)
+        channel = self.bot.get_channel(self.CHANNEL_ID) or await self.bot.fetch_channel(self.CHANNEL_ID)
+        guild = self.bot.get_guild(self.GUILD_ID) or await self.bot.fetch_guild(self.GUILD_ID)
         if guild == None or channel == None:
             print("Cannot find guild or channel")
             return
 
-        event = guild.get_scheduled_event(self.EVENT_ID)
+        event = guild.get_scheduled_event(self.EVENT_ID) or await guild.fetch_scheduled_event(self.EVENT_ID)
         if event == None:
             print("Cannot find event")
             return
 
-        td = event.start_time - datetime.now(timezone.utc)
+        dst_start = event.start_time + event.start_time.astimezone(ZoneInfo("US/Pacific")).dst() - timedelta(hours=1)
+        td = dst_start - datetime.now(timezone.utc)
         hours = int(td.seconds / 60 / 60)
-        if td.days == 0 and hours == 8:
-            await channel.send("<@&1085319290980409454> Next JERP session is <t:{}:R>!".format(int(event.start_time.timestamp())))
+        print("Days: ", td.days, "; Hours: ", hours, "; TS: ", int(dst_start.timestamp()))
+        if td.days == 0 and hours == 2:
+            await channel.send("<@&1085319290980409454> It's MILFing time <t:{}:R>!".format(int(dst_start.timestamp())))
 
     @send_reminder.before_loop
     async def before_printer(self):
